58eeae7ea9e2139dda333a6a1191a7d7e8904c55
MohamedAnis12 <vidotyobegaming@gmail.com>
2025-07-22 21:52:24 +0300

    add MultiBlocProvider for AddNoteCubit in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:todoapp/constants.dart';
+import 'package:todoapp/cubits/add_note_cubit/add_note_cubit_cubit.dart';
 import 'package:todoapp/models/note_model.dart';
 import 'package:todoapp/views/Notes_View.dart';
 
-void main() async{
-await Hive.initFlutter();
-await Hive.openBox(kNotesBox);
-Hive.registerAdapter(NoteModelAdapter());
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
 
   runApp(const NoteApp());
 }
@@ -17,10 +19,14 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NotesPage(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+
+      child: MaterialApp(
+        home: NotesPage(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+      ),
     );
   }
 }
