58a716a275a8ab63911f3da6abb82472ad47730d
MohamedAnis12 <vidotyobegaming@gmail.com>
2025-07-27 17:21:20 +0300

    fix: clean up formatting in AddNoteCubit and remove unused Kotlin compiler session file (final apk)

--- lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,18 +1,16 @@
 import 'dart:ui';
-
 import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:todoapp/constants.dart';
 import 'package:todoapp/models/note_model.dart';
-
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubit() : super(AddNoteCubitInitial());
-   Color color=    Color(0xffac3931);
+  Color color = Color(0xffac3931);
   addNote(NoteModel note) async {
-    note.color=color.value;
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNotesBox);
